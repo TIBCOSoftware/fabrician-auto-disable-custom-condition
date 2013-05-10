854c25c3ac42545407a0623184b1bb99090aecfc
Ethan Goodman <egoodman@tibco.com>
2013-05-10 16:02:50 -0400

    tabs to spaces

--- src/main/java/com/tibco/fabric/server/AutoDisableCustomCondition.java
+++ src/main/java/com/tibco/fabric/server/AutoDisableCustomCondition.java
@@ -9,77 +9,77 @@ import com.datasynapse.fabric.broker.userartifact.condition.AbstractCustomRuleCo
 
 public class AutoDisableCustomCondition extends AbstractCustomRuleCondition implements Serializable {
 
-	private static final long serialVersionUID = 2443519394918129355L;
-	
-	private String componentName = null;
-	private String autoDisable = "false";
-	private String waitFor = "10";
-	private int intWaitCount = 0;
-	private final ComponentAdmin ca = AdminManager.getComponentAdmin();
-	private String description = null;
-	private String lastModifiedBy = null;
+    private static final long serialVersionUID = 2443519394918129355L;
+    
+    private String componentName = null;
+    private String autoDisable = "false";
+    private String waitFor = "10";
+    private int intWaitCount = 0;
+    private final ComponentAdmin ca = AdminManager.getComponentAdmin();
+    private String description = null;
+    private String lastModifiedBy = null;
 
-	@Override
-	public String getDescription() {
-		return "Component " + componentName + " will activated for " +waitFor + " minutes and then shut down again";
-	}
+    @Override
+    public String getDescription() {
+        return "Component " + componentName + " will activated for " +waitFor + " minutes and then shut down again";
+    }
 
-	@Override
-	public boolean isSatisfied() {
-		boolean satisfied = false;
-		try {
-			// Allocated Engine Count in ComponentAdmin only returns successfully started Engines
-			int remEngineCountCA = ca.getAllocatedEngineCount(componentName);
-			if (autoDisable.equalsIgnoreCase("true") && remEngineCountCA == 0){
-				satisfied = true;
-				intWaitCount = Integer.parseInt(waitFor);
-			}
-			if (remEngineCountCA > 0){
-				// our component is now active stop waiting for activation and, begin the autodisable count down...
-				autoDisable = "false";
-				Logger.getLogger(getClass().getSimpleName()).fine("Counting down to disablement of " + componentName + " " + intWaitCount);
-				intWaitCount--;
-				if (intWaitCount > 0) {
-					satisfied = true;
-				}
+    @Override
+    public boolean isSatisfied() {
+        boolean satisfied = false;
+        try {
+            // Allocated Engine Count in ComponentAdmin only returns successfully started Engines
+            int remEngineCountCA = ca.getAllocatedEngineCount(componentName);
+            if (autoDisable.equalsIgnoreCase("true") && remEngineCountCA == 0){
+                satisfied = true;
+                intWaitCount = Integer.parseInt(waitFor);
+            }
+            if (remEngineCountCA > 0){
+                // our component is now active stop waiting for activation and, begin the autodisable count down...
+                autoDisable = "false";
+                Logger.getLogger(getClass().getSimpleName()).fine("Counting down to disablement of " + componentName + " " + intWaitCount);
+                intWaitCount--;
+                if (intWaitCount > 0) {
+                    satisfied = true;
+                }
 
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return satisfied;
-	}
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return satisfied;
+    }
 
-	public String getComponentName() {
-		return componentName;
-	}
+    public String getComponentName() {
+        return componentName;
+    }
 
-	public void setComponentName(String componentName) {
-		this.componentName = componentName;
-	}
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
+    }
 
-	public String getAutoDisable() {
-		return autoDisable;
-	}
+    public String getAutoDisable() {
+        return autoDisable;
+    }
 
-	public void setAutoDisable(String autoDisable) {
-		this.autoDisable = autoDisable;
-	}
+    public void setAutoDisable(String autoDisable) {
+        this.autoDisable = autoDisable;
+    }
 
-	public String getWaitFor() {
-		return waitFor;
-	}
+    public String getWaitFor() {
+        return waitFor;
+    }
 
-	public void setWaitFor(String waitFor) {
-		this.waitFor = waitFor;
-	}
+    public void setWaitFor(String waitFor) {
+        this.waitFor = waitFor;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setLastModifiedBy(String lastModifiedBy) {
-		this.lastModifiedBy = lastModifiedBy;
-	}
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
 
 }
